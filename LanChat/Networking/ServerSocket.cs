3b2131368e777e3a1ef17ed8e0e38a721a9c72c1
LcChinh21 <leducchinhdz@gmail.com>
2025-11-23 17:12:20 +0700

    commit

---
+++ LanChat/Networking/ServerSocket.cs
@@ -0,0 +1,15 @@
+﻿using System.Net.Sockets;
+
+wusing System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LanChat.Networking
+{
+    internal class ServerSocket
+    {
+        TcpListener
+    }
+}
